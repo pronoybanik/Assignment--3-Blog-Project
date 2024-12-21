15467da97468d24fe6df492463baaac4a3d82d8d
PRONOY <pronoybanik82@gmail.com>
2024-12-22 04:10:24 +0600

    solve error

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -5,48 +5,60 @@ import config from '../config';
 import AppError from './AppError';
 import { TErrorSources } from '../interface/error';
 import handleZodError from '../errors/handleZodError';
+import handleCastError from '../errors/handleCastError';
+import handleValidationError from '../errors/handleValidationError';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
-    let statusCode = 500;
-    let message = 'Something went wrong!';
+  let statusCode = 500;
+  let message = 'Something went wrong!';
 
-    let errorSources: TErrorSources = [
-        {
-            path: '',
-            message: 'Something went wrong!',
-        },
+  let errorSources: TErrorSources = [
+    {
+      path: '',
+      message: 'Something went wrong!',
+    },
+  ];
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'CastError') {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err instanceof AppError) {
+    statusCode = err?.statusCode;
+    message = err.message;
+    errorSources = [
+      {
+        path: '',
+        message: err?.message,
+      },
     ];
+  } else if (err instanceof Error) {
+    message = err.message;
+    errorSources = [
+      {
+        path: '',
+        message: err?.message,
+      },
+    ];
+  }
 
-    if (err instanceof ZodError) {
-        const simplifiedError = handleZodError(err);
-        statusCode = simplifiedError?.statusCode;
-        message = simplifiedError?.message;
-        errorSources = simplifiedError?.errorSources;
-    } else if (err instanceof AppError) {
-        statusCode = err?.statusCode;
-        message = err.message;
-        errorSources = [
-            {
-                path: '',
-                message: err?.message,
-            },
-        ];
-    } else if (err instanceof Error) {
-        message = err.message;
-        errorSources = [
-            {
-                path: '',
-                message: err?.message,
-            },
-        ];
-    }
-
-    return res.status(statusCode).json({
-        success: false,
-        message,
-        errorSources,
-        stack: config.NODE_ENV === 'development' ? err?.stack : null,
-    });
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    errorSources,
+    stack: config.NODE_ENV === 'development' ? err?.stack : null,
+  });
 };
 
 export default globalErrorHandler;
